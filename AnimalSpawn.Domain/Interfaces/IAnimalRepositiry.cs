6220426d2955e7cbd0f76e3dda6578973ae47ea4
CarlosBe115 <72365048+CarlosBe115@users.noreply.github.com>
2020-10-04 20:07:44 -0500

    Add files via upload

---
+++ AnimalSpawn.Domain/Interfaces/IAnimalRepositiry.cs
@@ -0,0 +1,15 @@
+﻿using AnimalSpawn.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalSpawn.Infraestructure.Repositories
+{
+    public interface IAnimalRepositiry
+    {
+        Task<IEnumerable<Animal>> GetAnimals();
+        Task<Animal> GetAnimal(int id);
+
+    }
+}
